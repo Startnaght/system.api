202056934e652f521833ea85c0135d45983c4e8e
changan <507566350@qq.com>
2025-03-15 14:53:30 +0800

    完成对数据的保存

--- src/main/java/com/star/imgapi/controller/CompleteController.java
+++ src/main/java/com/star/imgapi/controller/CompleteController.java
@@ -1,22 +1,23 @@
 package com.star.imgapi.controller;
 
+import java.io.BufferedReader;
 import java.util.List;
 
-import com.star.imgapi.serever.impl.uploadserverimpl;
-import com.star.imgapi.util.GobalLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.star.imgapi.entity.complete;
 import com.star.imgapi.serever.Mailservere;
-import com.star.imgapi.util.R;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import com.star.imgapi.entity.Uploadteam;
+import com.star.imgapi.util.GobalLog;
 import com.star.imgapi.util.IpUtil;
+import com.star.imgapi.util.R;
+import com.star.imgapi.util.UtilOsStarem;
+import com.star.imgapi.serever.impl.uploadserverimpl;
+import java.util.UUID;
 
 /**
  * @Author: oyster
@@ -26,7 +27,6 @@ import com.star.imgapi.util.IpUtil;
 @RestController
 @RequestMapping("/api")
 public class CompleteController {
-//访问路径ip:/main/..   
     @Autowired
     private Mailservere mailservere;
 
@@ -36,35 +36,33 @@ public class CompleteController {
     @RequestMapping("/findall")
     public R list() {
         List<complete> listAll = mailservere.findAll();
-
-        // return R.ok ().put("data",listALL);
         return R.ok().put("data", listAll);
     }
 
-    // @SuppressWarnings ()
     @RequestMapping("/text")
     public R text() {
         return R.ok().put("明天", "依旧");
     }
 
-    // RequestBody 只能用于json的数据的接受  包括表单数据
-    @PostMapping("/upload") //post请求 Uploadteam uploadteam)
-    public R upload(
-        @RequestParam("file") MultipartFile data,
-        @RequestParam("chunkIndex") int chunkIndex,
-        @RequestParam("fileName") String fileName
-    ) {  //接收前端传来的数据
+    @PostMapping("/upload")
+    public R upload(@RequestParam("file") MultipartFile data, @RequestParam("chunkIndex") String chunkIndex,
+        @RequestParam("fileName") String fileName) {
         IpUtil ipadder = new IpUtil();
-        System.out.println("接收到的数据: " +fileName);
-        GobalLog.info("数据已经成功上传~来自长安:",ipadder.toString());
-        uploadserverimpl uploadserverimpl = new uploadserverimpl(); //实例化
-        uploadserverimpl.Saveimg(data); //调用保存图片的方法
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));   // 获取文件后缀
+        String prefixName = fileName.substring(0, fileName.lastIndexOf("."));   // 获取文件名称不包含后缀
+        String uuid = UUID.randomUUID().toString().replace("-","");//创建服务器名称
+        uploadserverimpl uploadserveri = new uploadserverimpl();  // 构建具体对象
+        System.out.println("masses->接收到数据名称为：" + prefixName);   
+        GobalLog.info("数据已经成功上传~来自长安:", ipadder.toString());
+        // 图片编码为basee64 文本数据为utf-8
+        if (suffixName.equals(".txt")) {   // 如果后缀是文本文件就执行下面
+            uploadserveri.SaveTxt(data, fileName, chunkIndex);
+        } else if (suffixName.equals(".jpg") || suffixName.equals(".png")) {
+            uploadserveri.SaveImg(data, fileName);
+        } else {
+            return R.ok().put("message", "上传失败，服务器暂不支持此文件类型上传~");
+        }
 
-        return R.ok().put("上传成功，存储路径为：", 200);
+        return R.ok().put("message", "上传成功，存储路径为：path/to/save/" + fileName);
     }
-    
-    // public Integer request() {
-    //     return new Integer();
-    // }
-
 }
